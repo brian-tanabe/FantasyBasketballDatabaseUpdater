914f19a2faf20d8016f1117354681940805ebf83
Brian Tanabe <btanabe2@gmail.com>
2014-11-08 00:36:36 -0600

    Stubbed out empty project which will be responsible for updating the physical database.

--- DataProvider/src/main/java/com/btanabe2/fbdu/dp/providers/NbaTeamProvider.java
+++ DataProvider/src/main/java/com/btanabe2/fbdu/dp/providers/NbaTeamProvider.java
@@ -1,8 +1,17 @@
 package com.btanabe2.fbdu.dp.providers;
 
+import com.btanabe2.fbdu.dm.models.NbaTeamEntity;
+import com.btanabe2.fbdu.dp.scrapers.NbaTeamJsonParser;
+
+import java.io.IOException;
+import java.util.List;
+
 /**
  * Created by brian on 11/6/14.
  */
 public class NbaTeamProvider {
 
+    public static List<NbaTeamEntity> getAllNbaTeamEntities() throws IOException {
+        return NbaTeamJsonParser.getAllNbaTeams();
+    }
 }
---
+++ DatabaseUpdater/src/main/java/com/btanabe2/fbdu/du/application/Application.java
@@ -0,0 +1,7 @@
+package com.btanabe2.fbdu.du.application;
+
+/**
+ * Created by brian on 11/8/14.
+ */
+public class Application {
+}
