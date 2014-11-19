598e8eb8f5ad01fec64c74d5e69fa4c7a57d6b7f
Brian Tanabe <btanabe@zebra.com>
2014-11-19 16:31:07 -0600

    I found a new datasource.  This is the best one: it's RESTful

---
+++ DataProvider/src/main/java/com/btanabe2/fbdu/dp/stats/scrapers/PlayerProfileSportsVuScraper.java
@@ -0,0 +1,12 @@
+package com.btanabe2.fbdu.dp.stats.scrapers;
+
+/**
+ * Created by BTanabe on 11/19/2014.
+ */
+public class PlayerProfileSportsVuScraper {
+    // Check out this project:
+    // https://www.npmjs.org/package/nba
+
+    // All players: http://stats.nba.com/stats/commonallplayers?LeagueID=00&Season=2014-15&IsOnlyCurrentSeason=1
+
+}
