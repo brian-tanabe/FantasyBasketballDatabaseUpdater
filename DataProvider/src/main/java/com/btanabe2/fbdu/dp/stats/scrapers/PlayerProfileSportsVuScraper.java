4979c93c348c350badd1d1a33ccdf3ac542a1770
Brian Tanabe <btanabe@zebra.com>
2014-11-20 16:44:06 -0600

    Added some JSON parsing.  Still super incomplete but I have to leave!

--- DataProvider/src/main/java/com/btanabe2/fbdu/dp/stats/scrapers/PlayerProfileSportsVuScraper.java
+++ DataProvider/src/main/java/com/btanabe2/fbdu/dp/stats/scrapers/PlayerProfileSportsVuScraper.java
@@ -1,6 +1,8 @@
 package com.btanabe2.fbdu.dp.stats.scrapers;
 
-import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,11 +15,13 @@ public class PlayerProfileSportsVuScraper {
     // Check out this project:
     // https://www.npmjs.org/package/nba
 
+    // Aaron Afflalo game logs: http://stats.nba.com/stats/playerprofile?LeagueID=00&Season=2014-15&IsOnlyCurrentSeason=1&PlayerId=201167&GraphStat=PTS&GraphEndSeason=2014-15&GraphStartSeason=1990-91&SeasonType=Regular%20Season
     // All players: http://stats.nba.com/stats/commonallplayers?LeagueID=00&Season=2014-15&IsOnlyCurrentSeason=1
 
     public List<Map<String, String>> getAllActivePlayersNameIdAndExperience(String playerJson) {
-        Gson jsonParser = new Gson();
-        
+        JsonObject jsonElement = new JsonParser().parse(playerJson).getAsJsonObject();
+        JsonArray jsonArray = jsonElement.getAsJsonArray("resultSets");
+        JsonArray elements = (JsonArray) jsonArray.get(0).getAsJsonObject().get("rowSet");
 
         return new ArrayList<>();
     }
