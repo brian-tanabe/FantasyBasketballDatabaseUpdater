a9d059d01aea7e1eb8767aa33a4970ae19987362
Brian Tanabe <btanabe2@gmail.com>
2015-02-04 13:07:03 -0600

    Refactored WebRequestTests to not create a new WebRequest object for each request.

--- DataProvider/src/test/java/com/btanabe2/fbdu/dp/tests/unit/web/WebRequestTests.java
+++ DataProvider/src/test/java/com/btanabe2/fbdu/dp/tests/unit/web/WebRequestTests.java
@@ -1,6 +1,7 @@
 package com.btanabe2.fbdu.dp.tests.unit.web;
 
 import com.btanabe2.fbdu.dp.web.WebRequest;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -14,11 +15,17 @@ import static junit.framework.TestCase.fail;
  * Created by Brian on 11/27/14.
  */
 public class WebRequestTests {
+    private static WebRequest webRequest;
+
+    @BeforeClass
+    public static void setup() {
+        webRequest = new WebRequest();
+    }
 
     @Test
     public void shouldBeAbleToDownloadPlayerProfile(){
         try {
-            assertTrue("Downloaded page was empty", new WebRequest().getPage("http://stats.nba.com/stats/commonplayerinfo?PlayerID=203112").length() > 0);
+            assertTrue("Downloaded page was empty", webRequest.getPage("http://stats.nba.com/stats/commonplayerinfo?PlayerID=203112").length() > 0);
         } catch (IOException e) {
             e.printStackTrace();
             fail("Failed to download page");
@@ -28,12 +35,8 @@ public class WebRequestTests {
     @Test
     public void shouldBeAbleToMakeMultipleRequestsToDifferentDomains(){
         try {
-            WebRequest webRequest = new WebRequest();
-            String sportsVuPageString = webRequest.getPage(SPORTS_VU_ALL_PLAYERS_URL);
-            String numberFirePage = webRequest.getPage(NUMBER_FIRE_REMAINING_PROJECTIONS_FORWARDS_URL);
-
-            assertTrue("First downloaded page was empty", sportsVuPageString.length() > 0);
-            assertTrue("Second downloaded page was empty", numberFirePage.length() > 0);
+            assertTrue("First downloaded page was empty", webRequest.getPage(SPORTS_VU_ALL_PLAYERS_URL).length() > 0);
+            assertTrue("Second downloaded page was empty", webRequest.getPage(NUMBER_FIRE_REMAINING_PROJECTIONS_FORWARDS_URL).length() > 0);
         } catch (Exception ex){
             ex.printStackTrace();
             fail("Failed to download pages");
