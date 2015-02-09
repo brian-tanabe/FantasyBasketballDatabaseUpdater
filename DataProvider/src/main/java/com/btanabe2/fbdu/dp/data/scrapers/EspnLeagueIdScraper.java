c1e524a0c16ac31d184e606b9ffb142a7cb23f05
Brian Tanabe <btanabe2@gmail.com>
2015-02-08 23:18:07 -0600

    Finished implementing the league ID scraping that I wanted to do.

--- DataProvider/src/main/java/com/btanabe2/fbdu/dp/data/scrapers/EspnLeagueIdScraper.java
+++ DataProvider/src/main/java/com/btanabe2/fbdu/dp/data/scrapers/EspnLeagueIdScraper.java
@@ -4,8 +4,10 @@ import com.btanabe2.fbdu.dp.web.SecureWebRequest;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static com.btanabe2.fbdu.dp.web.WebConstants.ESPN_FANTASY_BASKETBALL_HOMEPAGE;
 
@@ -14,10 +16,27 @@ import static com.btanabe2.fbdu.dp.web.WebConstants.ESPN_FANTASY_BASKETBALL_HOME
  */
 public class EspnLeagueIdScraper {
 
+    /**
+     * Finds the ESPN league ID of the league whose name is passed into this
+     * function.  If that league name isn't present then the ID found will be
+     * returned.  If there are no leagues found then -1 will be returned.
+     *
+     * @param webRequest     A <class>SecureWebRequest</class> that has already
+     *                       authenticated with ESPN
+     * @param leagueNameHint League name to find its ID
+     * @return The league ID of the (1) parameterized league name, (2) the
+     * first league ID, or (3) -1 if no leagues were found
+     * @throws IOException thrown if errors occurred while making web requests.
+     */
     public int findCurrentSeasonFantasyLeagueId(SecureWebRequest webRequest, String leagueNameHint) throws IOException {
-        List<String> leagueUrls = findFantasyLeagueHomePageUrls(webRequest);
-
-        return -1;
+        Map<String, String> allFantasyLeagueUrlsMappedToTheirLeagueNames = getFantasyClubhouseLinksMappedToTheirLeagueNames(webRequest);
+        if (allFantasyLeagueUrlsMappedToTheirLeagueNames.containsKey(leagueNameHint)) {
+            return extractLeagueIdFromUrl(allFantasyLeagueUrlsMappedToTheirLeagueNames.get(leagueNameHint));
+        } else if (allFantasyLeagueUrlsMappedToTheirLeagueNames.size() > 0) {
+            return extractLeagueIdFromUrl(allFantasyLeagueUrlsMappedToTheirLeagueNames.get(allFantasyLeagueUrlsMappedToTheirLeagueNames.keySet().stream().limit(1).collect(Collectors.toList()).get(0)));
+        } else {
+            return -1;
+        }
     }
 
     /**
@@ -26,11 +45,19 @@ public class EspnLeagueIdScraper {
      * @throws IOException thrown if an exception occurs when making web requests
      */
     public List<String> findFantasyLeagueHomePageUrls(SecureWebRequest webRequest) throws IOException {
+        return getFantasyClubhouseLinksMappedToTheirLeagueNames(webRequest).values().stream().collect(Collectors.toList());
+    }
+
+    private Map<String, String> getFantasyClubhouseLinksMappedToTheirLeagueNames(SecureWebRequest webRequest) throws IOException {
         Elements leagueHomePageLinks = webRequest.getPageAsDocument(ESPN_FANTASY_BASKETBALL_HOMEPAGE).select("a.leagueoffice-link[href$=&seasonId=2015]");
 
-        List<String> links = new ArrayList<>();
-        leagueHomePageLinks.stream().forEach(l -> links.add(l.attr("href")));
+        Map<String, String> linksMappedToTheirLeagueName = new HashMap<>();
+        leagueHomePageLinks.stream().forEach(l -> linksMappedToTheirLeagueName.put(l.text(), l.attr("href")));
 
-        return links;
+        return linksMappedToTheirLeagueName;
+    }
+
+    private int extractLeagueIdFromUrl(String url) {
+        return Integer.parseInt(url.substring(url.indexOf("leagueId="), url.indexOf("&")).replaceAll("[^\\d]", ""));
     }
 }
