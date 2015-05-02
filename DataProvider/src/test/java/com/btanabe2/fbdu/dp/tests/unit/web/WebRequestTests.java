bb041be16f42c388d9db1c5f7139e1b89397744b
Brian Tanabe <btanabe2@gmail.com>
2015-05-02 15:31:36 -0500

    Killed the @BeforeClass here.

--- DataProvider/src/test/java/com/btanabe2/fbdu/dp/tests/unit/web/WebRequestTests.java
+++ DataProvider/src/test/java/com/btanabe2/fbdu/dp/tests/unit/web/WebRequestTests.java
@@ -1,8 +1,11 @@
 package com.btanabe2.fbdu.dp.tests.unit.web;
 
 import com.btanabe2.fbdu.dp.web.WebRequest;
-import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.IOException;
 
@@ -14,13 +17,11 @@ import static junit.framework.TestCase.fail;
 /**
  * Created by Brian on 11/27/14.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring-configuration/application.xml")
 public class WebRequestTests {
-    private static WebRequest webRequest;
-
-    @BeforeClass
-    public static void setup() {
-        webRequest = new WebRequest();
-    }
+    @Autowired
+    private WebRequest webRequest;
 
     @Test
     public void shouldBeAbleToDownloadPlayerProfile(){
